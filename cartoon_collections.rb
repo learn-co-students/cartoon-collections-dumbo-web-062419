f614a61ed3044ec7136ed1abe42c384a4d95a1bd
lgm527 <laurellmccaffrey@gmail.com>
2019-06-03 10:45:41 -0400

    cartoon collections have been collected

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,25 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves(list)
+  list.each_with_index do |x, i|
+    puts "#{i+1}. #{x}"
+  end
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(calls)
+  calls.map { |call|
+    call.gsub(/\w+/, &:capitalize) << "!"
+  }
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(calls)
+  calls.any? do |call|
+    call.size > 4
+  end
 end
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
+def find_the_cheese(list)
   cheese_types = ["cheddar", "gouda", "camembert"]
+  cheese_types.each do |cheese|
+    return cheese if list.include?(cheese)
+  end
+  return nil
 end
